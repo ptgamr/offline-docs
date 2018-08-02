adf494477a781042dfc50d5f7aaaaf2540117e68
Anh Trinh <anh.trinhtrung@gmail.com>
2018-08-02 17:22:56 +0700

    change title

--- src/App.js
+++ src/App.js
@@ -159,7 +159,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Offline Docs</h1>
+          <h1 className="App-title">PWA Prototype</h1>
         </header>
         <p></p>
         <div className="App-intro">
